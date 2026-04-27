fe450ddafd8c20702261b2a6adb515802b1c7a8f
p30s08 <p30s08v75@yandex.ru>
2026-04-27 22:43:47 +0300

    Добавлена реализация запроса конкретного экхамена по идентификатору

--- Realisation/Back/services/exams.py
+++ Realisation/Back/services/exams.py
@@ -6,3 +6,14 @@ def get_available_exams() -> list[int]:
     Берём из того же хранилища куда сохраняли вопросы.
     """
     return list(range(1, len(QUESTIONS_STORAGE) + 1))  
+def get_exam_by_id(exam_id: int) -> dict | None:
+   
+    """
+    Возвращаем данные экзамена по ID.
+    
+    """
+    if exam_id < 1 or exam_id > len(QUESTIONS_STORAGE):
+        return None  # экзамен с таким ID не найден
+  
+
+    return QUESTIONS_STORAGE[exam_id - 1]  # достаём из базы данных
