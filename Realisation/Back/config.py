80b7aa2401918d78cc8bba7c4819bd4567c545fc
p30s08 <p30s08v75@yandex.ru>
2026-05-08 09:34:24 +0300

    Добавлена реализация выбора модели

--- Realisation/Back/config.py
+++ Realisation/Back/config.py
@@ -9,16 +9,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+GIGA_API_KEY = os.getenv("GIGA_API_KEY")
 
-# пробуем прочитать ключ из api_keys.yml
-def _load_from_yaml() -> str | None:
-    try:
-        with open("api_keys.yml", "r", encoding="utf-8") as f:
-            data = yaml.safe_load(f)
-            return data.get("gigachat_api_key")
-    except FileNotFoundError:
-        return None
+# читаем какую LLM использовать
+with open("settings.yaml", "r", encoding="utf-8") as f:
+    LLM_PROVIDER = yaml.safe_load(f).get("llm_provider", "local")
 
 
-# сначала берём из yml, если нет — из .env
-GIGA_API_KEY = _load_from_yaml() or os.getenv("GIGA_API_KEY")
+
+# # пробуем прочитать ключ из api_keys.yml
+# def _load_from_yaml() -> str | None:
+#     try:
+#         with open("api_keys.yml", "r", encoding="utf-8") as f:
+#             data = yaml.safe_load(f)
+#             return data.get("gigachat_api_key")
+#     except FileNotFoundError:
+#         return None
+
+
+# # сначала берём из yml, если нет — из .env
+# GIGA_API_KEY = _load_from_yaml() or os.getenv("GIGA_API_KEY")
